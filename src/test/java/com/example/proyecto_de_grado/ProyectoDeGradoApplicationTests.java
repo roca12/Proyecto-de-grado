76c080e7a9abfcfcc5a328c01e7ab90d875ecfdf
Santiago Arias Ayala <106265476+helloWorld3103@users.noreply.github.com>
2025-03-26 15:06:47 -0500

    Add files via upload

---
+++ src/test/java/com/example/proyecto_de_grado/ProyectoDeGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_de_grado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDeGradoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
