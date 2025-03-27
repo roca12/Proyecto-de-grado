7156a261018657893201114186847f597bfc3792
Anderson <fabianzuluaganeita@gmail.com>
2025-03-27 11:35:50 -0500

    creation of classes & configuration

---
+++ src/main/java/com/example/proyecto_de_grado/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package com.example.proyecto_de_grado.service;
+
+import com.example.proyecto_de_grado.model.entity.Usuario;
+import com.example.proyecto_de_grado.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getAllUsuarios() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        System.out.println("Usuarios encontrados: " + usuarios);
+        return usuarios;
+    }
+
+    public Optional<Usuario> getUsuarioById(int id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario saveUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deleteUsuario(int id) {
+        usuarioRepository.deleteById(id);
+    }
+}
